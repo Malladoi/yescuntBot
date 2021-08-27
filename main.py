5cf477cdaf902e5353748eeaf69f9a4a072d8c1c
Malladoi <iloveyoubutyoudontcare@gmail.com>
2021-08-27 16:42:12 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import telebot
+import logging
+
+token = os.environ["TELEGRAM_TOKEN"]
+api_token = os.environ["API_TOKEN"]
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.WARNING)  # Outputs debug messages to console.
+bot = telebot.AsyncTeleBot(token + ":" + api_token)
+
+
+@bot.message_handler(content_types=["text"])
+def sendcunt(message):
+    bot.send_sticker(message.chat.id,
+                     'CAACAgIAAxkBAAIEcWEo5-u9aIvKB5C0W5hGpuVD9BoIAALjEgAC9dC2HQhKdZuwAd7OIAQ',
+                     message.id)
+
+
+bot.polling(none_stop=True)
