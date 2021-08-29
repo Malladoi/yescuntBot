50545e3968f38b11ed3c21078bffad078032c304
Malladoi <iloveyoubutyoudontcare@gmail.com>
2021-08-29 22:10:03 +0300

    add forecast comm

--- main.py
+++ main.py
@@ -15,27 +15,48 @@ bot = telebot.AsyncTeleBot(token + ":" + api_token)
 @bot.message_handler(commands=["weather"])
 def weather(message):
     try:
-        city = message.text.replace("/weather ", "")
-        resp = wthr.weatherbycity(city)
-        if resp.status_code == 200:
-            dict_str = resp.content.decode("UTF-8")
-            data = ast.literal_eval(dict_str)
-        bot.reply_to(message, "{0}\n"
+        if message.text == "/weather":
+            markup = types.ForceReply(selective=False)
+            bot.send_message(message.chat.id, "Напиши мне название города или населенного пункта", reply_markup=markup)
+        else:
+            city = message.text.replace("/weather ", "")
+            resp = wthr.weatherbycity(city)
+            if resp.status_code == 200:
+                dict_str = resp.content.decode("UTF-8")
+                wdata = ast.literal_eval(dict_str)
+            bot.reply_to(message, "{0}\n"
                               "Темп. {1}\n"
                               "Ощущается как {2}\n"
                               "Мин {3}\n"
                               "Макс {4}\n"
-                              "Влажность {5}%".format(data['weather'][0]['description'],
-                                                      data['main']['temp'],
-                                                      data['main']['feels_like'],
-                                                      data['main']['temp_min'],
-                                                      data['main']['temp_max'],
-                                                      data['main']['humidity']))
+                              "Влажность {5}%".format(wdata['weather'][0]['description'],
+                                                      wdata['main']['temp'],
+                                                      wdata['main']['feels_like'],
+                                                      wdata['main']['temp_min'],
+                                                      wdata['main']['temp_max'],
+                                                      wdata['main']['humidity']))
     except Exception as e:
         print(e)
 
 @bot.message_handler(content_types=["text"])
 def sendcunt(message):
+    if (message.reply_to_message.text == 'Напиши мне название города или населенного пункта' and message.reply_to_message.from_user.username == 'Winkies_bot'):
+        city = message.text
+        resp = wthr.weatherbycity(city)
+        if resp.status_code == 200:
+            dict_str = resp.content.decode("UTF-8")
+            wdata = ast.literal_eval(dict_str)
+        bot.reply_to(message, "{0}\n"
+                              "Темп. {1}\n"
+                              "Ощущается как {2}\n"
+                              "Мин {3}\n"
+                              "Макс {4}\n"
+                              "Влажность {5}%".format(wdata['weather'][0]['description'],
+                                                      wdata['main']['temp'],
+                                                      wdata['main']['feels_like'],
+                                                      wdata['main']['temp_min'],
+                                                      wdata['main']['temp_max'],
+                                                      wdata['main']['humidity']))
     if str(message.text).lower() == 'да':
         bot.send_sticker(message.chat.id,
                          'CAACAgIAAxkBAAIEcWEo5-u9aIvKB5C0W5hGpuVD9BoIAALjEgAC9dC2HQhKdZuwAd7OIAQ',
@@ -48,32 +69,32 @@ def query_text(inline_query):
         resp = wthr.weather(inline_query)
         if resp.status_code == 200:
             dict_str = resp.content.decode("UTF-8")
-            data = ast.literal_eval(dict_str)
+            wdata = ast.literal_eval(dict_str)
             re = types.InlineQueryResultArticle(id='1',
-                                                title="{0}".format(data['weather'][0]['description']),
-                                                thumb_url="https://openweathermap.org/img/wn/{0}@2x.png".format(data['weather'][0]['icon']),
+                                                title="{0}".format(wdata['weather'][0]['description']),
+                                                thumb_url="https://openweathermap.org/img/wn/{0}@2x.png".format(wdata['weather'][0]['icon']),
                                                 input_message_content=types.InputTextMessageContent("{0}\n"
                                                             "Темп. {1}\n"
                                                             "Ощущается как {2}\n"
                                                             "Мин {3}\n"
                                                             "Макс {4}\n"
-                                                            "Влажность {5}%".format(data['weather'][0]['description'],
-                                                                                    data['main']['temp'],
-                                                                                    data['main']['feels_like'],
-                                                                                    data['main']['temp_min'],
-                                                                                    data['main']['temp_max'],
-                                                                                    data['main']['humidity'])),
+                                                            "Влажность {5}%".format(wdata['weather'][0]['description'],
+                                                                                    wdata['main']['temp'],
+                                                                                    wdata['main']['feels_like'],
+                                                                                    wdata['main']['temp_min'],
+                                                                                    wdata['main']['temp_max'],
+                                                                                    wdata['main']['humidity'])),
                                                 description="{0}\n"
                                                             "Темп. {1}\n"
                                                             "Ощущается как {2}\n"
                                                             "Мин {3}\n"
                                                             "Макс {4}\n"
-                                                            "Влажность {5}%".format(data['weather'][0]['description'],
-                                                                                    data['main']['temp'],
-                                                                                    data['main']['feels_like'],
-                                                                                    data['main']['temp_min'],
-                                                                                    data['main']['temp_max'],
-                                                                                    data['main']['humidity']))
+                                                            "Влажность {5}%".format(wdata['weather'][0]['description'],
+                                                                                    wdata['main']['temp'],
+                                                                                    wdata['main']['feels_like'],
+                                                                                    wdata['main']['temp_min'],
+                                                                                    wdata['main']['temp_max'],
+                                                                                    wdata['main']['humidity']))
             # r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
             bot.answer_inline_query(inline_query.id, [re])
     except Exception as e:
